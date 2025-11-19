9b99da1326ac98893a553414ef9f0f393e9a8498
Rajib Rawal <rajibrawal.rr@gmail.com>
2025-11-19 16:35:57 +0530

    Create user.py

---
+++ app/schemas/user.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    name: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    name: str
+
+    class Config:
+        from_attributes = True
+
+class LoginSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+class TokenSchema(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
