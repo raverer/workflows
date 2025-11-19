fea479c2e5175d845c492d28172b90e915e0cf1e
Rajib Rawal <rajibrawal.rr@gmail.com>
2025-11-19 16:47:28 +0545

    Add files via upload

---
+++ app/db/session.py
@@ -0,0 +1,24 @@
+# app/db/session.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.db.base import Base
+import app.db.base_models  # <── IMPORTANT import
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
