8db7c0767cf2d290288b175ff191b293ad61aa49
Rajib Rawal <rajibrawal.rr@gmail.com>
2025-11-20 11:27:37 +0530

    Update base.py

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,7 @@
-from app.db.base_class import Base
-from app.db.models.user import User
+# app/db/base.py
+
+from .base_class import Base  # <-- FIXED IMPORT
 from app.db.models.trend import Trend
+from app.db.models.user import User
+
+# This file ensures all models are imported so Base.metadata.create_all works
