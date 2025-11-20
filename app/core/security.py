4d7678fe68dac71b54de9f83a71b1112840f11f5
Rajib Rawal <rajibrawal.rr@gmail.com>
2025-11-20 11:40:52 +0530

    Update security.py

--- app/core/security.py
+++ app/core/security.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime, timedelta
 from typing import Optional
-from jose import jwt, JWTError
+from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import HTTPException, status, Depends
 
