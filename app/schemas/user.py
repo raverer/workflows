e842c78568c2c80d43c54a484ee21d164984748c
Rajib Rawal <rajibrawal.rr@gmail.com>
2025-11-20 20:06:11 +0530

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,21 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
-    email: str
+    email: EmailStr
     name: str
     password: str
 
 class UserResponse(BaseModel):
     id: int
-    email: str
+    email: EmailStr
     name: str
 
     class Config:
         from_attributes = True
-
-class LoginSchema(BaseModel):
-    email: str
-    password: str
 
 class TokenSchema(BaseModel):
     access_token: str
